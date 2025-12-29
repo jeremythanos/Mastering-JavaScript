cbbf57eaf6ac461c14b82573cfda70e78fa202d0
paulinotav1017 <paulinotav1017@gmail.com>
2025-12-29 00:15:18 +0000

    cambios de clase y aplicar estilos en css cuando la ventana cambia

--- JavaScript Senior level/Capitulo 16/MatchMedia/MatchMedia.js
+++ JavaScript Senior level/Capitulo 16/MatchMedia/MatchMedia.js
@@ -1,2 +1,12 @@
-"use strict";
+"use strict"
+
+const mq = matchMedia("(max-width: 500px)");
+const caja = document.querySelector(".caja");
+
+
+mq.addEventListener("change", ()=>{
+    if (mq.matches) caja.classList.replace("caja","responsive-caja");
+    else if (caja.className == "responsive-caja") caja.classList.replace("responsive-caja", "caja")
+})
+
 
